8ae87f2e915fb91031fe6c961c7d2644127f0ed3
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-02-16 23:05:50 +0900

    commit signup & policy setting

---
+++ app/src/main/java/com/uos/smsmsm/activity/policy/PolicyActivity.kt
@@ -0,0 +1,12 @@
+package com.uos.smsmsm.activity.policy
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.uos.smsmsm.R
+
+class PolicyActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_policy)
+    }
+}
