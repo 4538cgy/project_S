88409d0a33026a491612620f113018b0c49384c3
jeongupark <jeongu@getaim.co>
2021-03-07 16:15:10 +0900

    사용되는 화면 예시 주석으로 작성

--- app/src/main/java/com/uos/smsmsm/viewmodel/CommunicateViewModel.kt
+++ app/src/main/java/com/uos/smsmsm/viewmodel/CommunicateViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
 // 채팅이나 sns등 user들 소통관련 viewModel
+// ex)chatRoom, FriendList, SearchFreind, Lobby
 class ChatRoomViewModel @Inject constructor(): ViewModel() {
 
     fun getChatRoomInfo(){
--- app/src/main/java/com/uos/smsmsm/viewmodel/UserViewModel.kt
+++ app/src/main/java/com/uos/smsmsm/viewmodel/UserViewModel.kt
@@ -4,6 +4,7 @@ import androidx.lifecycle.ViewModel
 import javax.inject.Inject
 
 // 로그인이나 기타 사용자 관련 ViewModel
+// ex) Login, Policy, Seeing, SignUp
 class UserViewModel @Inject constructor() : ViewModel() {
 
 }
