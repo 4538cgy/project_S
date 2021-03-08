04e24e7acfc913ce76dc03abcee86e1409f3489c
jeongupark <jeongu@getaim.co>
2021-03-09 01:22:50 +0900

    필요 뷰모델 과 hilt 필요 내용 적용

---
+++ app/src/main/java/com/uos/smsmsm/viewmodel/SNSUtilViewModel.kt
@@ -0,0 +1,10 @@
+package com.uos.smsmsm.viewmodel
+
+import androidx.hilt.lifecycle.ViewModelInject
+import androidx.lifecycle.ViewModel
+
+// 채팅 / Timeline / 친구 찾기등 소셜 네트워크 기능 viewmodel
+class SNSUtilViewModel @ViewModelInject constructor() : ViewModel(){
+
+
+}
