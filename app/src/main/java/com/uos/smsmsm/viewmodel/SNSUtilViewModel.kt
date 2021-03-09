774a2aa08bc28196a6de03528758cb348e1a9bb5
4538cgy <4538cgy@gmail.com>
2021-03-09 21:57:37 +0900

    clean code

--- app/src/main/java/com/uos/smsmsm/viewmodel/SNSUtilViewModel.kt
+++ app/src/main/java/com/uos/smsmsm/viewmodel/SNSUtilViewModel.kt
@@ -15,24 +15,7 @@ class SNSUtilViewModel @ViewModelInject constructor( @Assisted private val saved
 
     fun initChatRoomList(){
         var list : ArrayList<RecyclerDefaultModel> = arrayListOf()
-        /* 데이터 가져와서 list에 꽂아넣기
-        val list2 = listOf(
-        RecyclerDefaultModel(
-                RecyclerDefaultModel.FRIENDS_LIST_TYPE_TITLE,
-                "https://firebasestorage.googleapis.com/v0/b/project-s-8efd0.appspot.com/o/TestImage%2FTEST_IMAGE_2021%EB%85%84%2002%EC%9B%94%2028%EC%9D%BC%20%EC%98%A4%ED%9B%84%2003%EC%8B%9C%2051%EB%B6%84%2048%EC%B4%88_.png?alt=media&token=d5429bc4-bee5-4be8-a7e6-82e3989913fe",
-                null,
-                "퀸더블랙",
-                ""
-            ),
-            RecyclerDefaultModel(
-                RecyclerDefaultModel.TEXT_TYPE,
-                "",
-                null,
-                "친구 62",
-                ""
-            )
-         )
-         */
+
         recyclerData.postValue(list)
     }
 
