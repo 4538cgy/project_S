791b09172928bf9b0ea60c344b237f3601c0d4c3
4538cgy <4538cgy@gmail.com>
2021-03-09 18:46:34 +0900

    commit 상단 bar

--- app/src/main/java/com/uos/smsmsm/fragment/tabmenu/chatroom/ChatRoomFragment.kt
+++ app/src/main/java/com/uos/smsmsm/fragment/tabmenu/chatroom/ChatRoomFragment.kt
@@ -1,9 +1,8 @@
 package com.uos.smsmsm.fragment.tabmenu.chatroom
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
+import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.uos.smsmsm.R
@@ -19,7 +18,35 @@ class ChatRoomFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View {
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_chat_room, container, false)
+        binding.fragmentchatroom = this
+        setHasOptionsMenu(true)
+        (activity as AppCompatActivity).setSupportActionBar(binding.fragmentChatRoomToolbar)
+        var actionBar = (activity as AppCompatActivity).supportActionBar
+        actionBar?.setDisplayShowTitleEnabled(false)
 
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.menu_option_bar,menu)
+
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_search ->{
+                return true
+            }
+            R.id.action_add-> {
+                return true
+            }
+            R.id.action_another ->{
+                return true
+            }
+            else -> return super.onOptionsItemSelected(item)
+        }
     }
 }
