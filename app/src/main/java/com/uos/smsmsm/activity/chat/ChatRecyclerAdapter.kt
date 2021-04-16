812a7d72c92f091a67f1ee51dc94b158d22c0743
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-04-16 12:42:31 +0900

    commit chat adapter complete #1

--- app/src/main/java/com/uos/smsmsm/activity/chat/ChatRecyclerAdapter.kt
+++ app/src/main/java/com/uos/smsmsm/activity/chat/ChatRecyclerAdapter.kt
@@ -6,15 +6,24 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.RequestOptions
 import com.google.firebase.auth.FirebaseAuth
 import com.uos.smsmsm.R
 import com.uos.smsmsm.data.ChatDTO
 import com.uos.smsmsm.databinding.ItemChatBubbleBinding
+import com.uos.smsmsm.repository.UserRepository
 import com.uos.smsmsm.util.time.TimeUtil
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
 
-class ChatRecyclerAdapter(private var context: Context, private val list : LiveData<ArrayList<ChatDTO.Comment>>) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+class ChatRecyclerAdapter(private var context: Context, private val list : LiveData<ArrayList<ChatDTO.Comment>>,private val destinationUid : String) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
+    val userRepository = UserRepository()
     val uid = FirebaseAuth.getInstance().currentUser!!.uid
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
@@ -34,9 +43,17 @@ class ChatRecyclerAdapter(private var context: Context, private val list : LiveD
             holder.binding.messageItemLinearlayoutMain.gravity = Gravity.RIGHT
         }else{
             //프로필 이미지 가져오고
-
             //유저 닉네임 가져오고
-
+            runBlocking {
+                launch {
+                    userRepository.getUserProfileImage(destinationUid).collect{
+                        Glide.with(holder.binding.root.context).load(it).apply(RequestOptions().circleCrop()).into(holder.binding.messageItemImageviewProfile)
+                    }
+                    userRepository.getUserNickName(destinationUid).collect {
+                        holder.binding.messageItemTextviewName.text = it
+                    }
+                }
+            }
             holder.binding.messageItemLinearlayoutDestination.visibility = View.VISIBLE
             holder.binding.messageItemTextViewMessage.setBackgroundResource(R.drawable.background_round_gray)
             holder.binding.messageItemTextViewMessage.text = list.value!![position].message
