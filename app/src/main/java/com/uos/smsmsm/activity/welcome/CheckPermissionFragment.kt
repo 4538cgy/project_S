d55f7c833f26484f6dedf656eb8503ef6e824d3e
jeongupark <jeongupark@gmail.com>
2021-04-30 02:18:52 +0900

    welcome페이지에서 권한 요청 하도록 기능 추가

---
+++ app/src/main/java/com/uos/smsmsm/activity/welcome/CheckPermissionFragment.kt
@@ -0,0 +1,21 @@
+package com.uos.smsmsm.activity.welcome
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.uos.smsmsm.R
+
+class CheckPermissionFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_permission_check, container, false)
+    }
+
+
+
+}
--- app/src/main/java/com/uos/smsmsm/activity/welcome/WelcomeMainActivity.kt
+++ app/src/main/java/com/uos/smsmsm/activity/welcome/WelcomeMainActivity.kt
@@ -4,6 +4,8 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
@@ -12,11 +14,13 @@ import androidx.viewpager2.widget.ViewPager2
 import com.uos.smsmsm.R
 import com.uos.smsmsm.activity.login.LoginActivity
 import com.uos.smsmsm.databinding.ActivityWelcomeMainBinding
+import com.uos.smsmsm.util.Config
+import com.uos.smsmsm.util.isPermitted
 import com.uos.smsmsm.util.shareddate.PreferenceUtil
 
-private const val NUM_PAGES = 3
-
 class WelcomeMainActivity : FragmentActivity() {
+
+    private val NUM_PAGES = 4
 
     lateinit var binding : ActivityWelcomeMainBinding
 
@@ -28,7 +32,8 @@ class WelcomeMainActivity : FragmentActivity() {
         binding.activityWelcomeMainIndicator.setViewPager(binding.activityWelcomePageViewpager)
         binding.activityWelcomePageViewpager.registerOnPageChangeCallback(object : ViewPager2.OnPageChangeCallback(){
             override fun onPageSelected(position: Int) {
-                if (position == 2){binding.activityWelcomeMainButtonAdultCheck.visibility = View.VISIBLE} else{ binding.activityWelcomeMainButtonAdultCheck.visibility = View.GONE}
+                if (position == 3){binding.activityWelcomeMainButtonAdultCheck.visibility = View.VISIBLE} else{ binding.activityWelcomeMainButtonAdultCheck.visibility = View.GONE}
+                if(position == 2){ binding.activityWelcomeMainButtonPermissionCheck.visibility = View.VISIBLE} else{ binding.activityWelcomeMainButtonPermissionCheck.visibility = View.GONE}
                 super.onPageSelected(position)
             }
         })
@@ -40,17 +45,43 @@ class WelcomeMainActivity : FragmentActivity() {
             when(position){
                 0 -> {fragment = WelcomeFragment() }
                 1 -> {fragment = IntroFragment() }
-                2 -> {fragment = AdultCheckFragment()
-                }
+                2 -> {fragment = CheckPermissionFragment()}
+                3 -> {fragment = AdultCheckFragment() }
             }
             return fragment
         }
     }
-
+    private fun moveNext(){
+        binding.activityWelcomePageViewpager.setCurrentItem(3,true)
+    }
     fun onClickAccept(view : View){
         PreferenceUtil(binding.root.context).setString("adultCheck","true")
         startActivity(Intent(binding.root.context, LoginActivity::class.java))
         finish()
     }
-
+    fun onRequestPermission(v: View){
+        if (isPermitted( applicationContext, Config.WELCOME_REQUEST_PERMISSION)) {
+            moveNext()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, Config.WELCOME_REQUEST_PERMISSION,
+                Config.FLAG_WELCOME_REQUEST_PERMISSION
+            )
+        }
+    }
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == Config.FLAG_WELCOME_REQUEST_PERMISSION) {
+            if (isPermitted(applicationContext, Config.WELCOME_REQUEST_PERMISSION)) {
+               moveNext()
+            } else {
+                Toast.makeText(applicationContext, "Permissions not granted by the user.", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
+    }
 }
