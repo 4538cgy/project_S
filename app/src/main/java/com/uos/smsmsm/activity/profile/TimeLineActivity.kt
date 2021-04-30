7a3da9163b2d176e99e63f49c7ca471611e7afaf
4538cgy <58514224+4538cgy@users.noreply.github.com>
2021-04-30 20:28:06 +0900

    commit fix timeline activity #1

--- app/src/main/java/com/uos/smsmsm/activity/profile/TimeLineActivity.kt
+++ app/src/main/java/com/uos/smsmsm/activity/profile/TimeLineActivity.kt
@@ -5,21 +5,26 @@ import android.os.Bundle
 import android.view.View
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.viewModels
 import com.uos.smsmsm.R
 import com.uos.smsmsm.databinding.ActivityTimeLineBinding
 import com.uos.smsmsm.viewmodel.ContentUtilViewModel
+import com.uos.smsmsm.viewmodel.UserUtilViewModel
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class TimeLineActivity : AppCompatActivity() {
 
     lateinit var binding : ActivityTimeLineBinding
-    private val viewModel : ContentUtilViewModel by viewModels()
+    private val viewModel: ContentUtilViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_time_line)
-        binding.activitytimeline = this@TimeLineActivity
-        binding.lifecycleOwner = this
         binding.viewmodel = viewModel
+        binding.activitytimeline = this@TimeLineActivity
+        binding.lifecycleOwner = this@TimeLineActivity
+
 
 
     }
