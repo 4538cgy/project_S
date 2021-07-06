fbef965a2f87b835efcddb79125971e6f8c69165
jeongupark <jeongupark@gmail.com>
2021-07-06 11:52:48 +0900

    gitignore 다시 적용

--- app/src/main/java/com/uos/smsmsm/util/OnClickListener.kt
+++ app/src/main/java/com/uos/smsmsm/util/OnClickListener.kt
@@ -6,17 +6,14 @@ import android.view.View
 abstract class OneClickListener : View.OnClickListener {
     override fun onClick(v: View) {
         if (started || SystemClock.elapsedRealtime() - lastClickEndTime < 500) {
-            //Log.d(OneClickListener.class.toString(), "Rejected double click, " + new Date().toString() );
             return
         }
-        //Log.d(OneClickListener.class.toString(), "One click, start: " + new Date().toString() );
         try {
             started = true
             onOneClick(v)
         } finally {
             started = false
             lastClickEndTime = SystemClock.elapsedRealtime()
-            //Log.d(OneClickListener.class.toString(), "One click, end: " + new Date().toString() );
         }
     }
 
