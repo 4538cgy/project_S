f722278161dd772df5b495ea38858e982e2e18e6
jeongupark <jeongupark@gmail.com>
2021-07-28 18:19:39 +0900

    FAQ base 생성

---
+++ app/src/main/java/com/uos/smsmsm/fragment/customersupport/FQAFragment.kt
@@ -0,0 +1,11 @@
+package com.uos.smsmsm.fragment.customersupport
+
+import com.uos.smsmsm.R
+import com.uos.smsmsm.base.BaseFragment
+import com.uos.smsmsm.databinding.FragmentFaqBinding
+
+class FQAFragment : BaseFragment<FragmentFaqBinding>(R.layout.fragment_faq) {
+
+
+
+}
